563bc29586a52161fff504501415e4dfeb8ff8f9
Kookheon Kim <kookheon.kim@ndotlight.com>
2022-05-19 17:41:41 +0900

    Add .env dummy

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () =>
       await createConnection({
         type: 'mysql',
-        host: 'localhost',
+        host: process.env.DB_HOST,
         port: 3306,
-        username: 'root',
-        password: '1234',
-        database: 'test',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
